5cf6ca336162185d4f345cb67f354d30ded1697c
maxbehzadi82 <maxbehzadi82@gmail.com>
2021-08-05 03:47:01 +0200

    first message and location with google map sent

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,26 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated', count);
+socket.on('message', (message) => {
+    console.log(message);
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked');
-    socket.emit('increment');
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const message = e.target.elements.msg.value;
+
+    socket.emit('sendMessage', message)
+
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation)
+        return alert('Geolocation is not available.')
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -21,20 +21,25 @@ const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
 
-
-let count = 0;
 io.on('connection', (socket) => {
     console.log('New Websocket connection');
 
-    socket.emit('countUpdated', count);
-    
-    socket.on('increment', () => {
-        count++;
-        // socket.emit('countUpdated', count);
-        io.emit('countUpdated', count);
+    socket.emit('message', "welcome to Chitchat");
+    socket.broadcast.emit('message','User is online!');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User is offline');
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://maps.google.com/?q=${coords.latitude},${coords.longitude}`)
     })
 })
 
 server.listen(port, () => {
     console.log(`Server on port ${port}!`);
-});
+})
