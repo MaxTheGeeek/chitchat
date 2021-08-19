f891bdf0b982b3f550820ca878331bea2dd9228e
maxbehzadi82 <maxbehzadi82@gmail.com>
2021-08-19 10:26:31 +0200

    App UI added,Join form added

--- src/index.js
+++ src/index.js
@@ -1,56 +1,46 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
+const path = require('path')
+const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
-const {generateMessage , generateLocationMessage} = require('./utils/messages')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
-//call express package
-const app = express();
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-//Allow us to create new server manually and refactoring in app
-const server = http.createServer(app);
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-//Create server to support socketio
-const io = socketio(server);
-
-const port = process.env.PORT || 3000;
-
-// Join public folder to project directory
-const publicPath = path.join(__dirname, '../public');
-
-// Use middleware for factorization public folder from routes
-app.use(express.static(publicPath));
-
+app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log('New Websocket connection');
+    console.log('New WebSocket connection')
 
-    //inform other users when user joins the chat ans send welcome msg to user
-    socket.emit('message', generateMessage('Welcome!'));
-    socket.broadcast.emit('message', generateMessage('User is online!'));
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
-    socket.on('sendMessage', (message , callback) => {
-        io.emit('message', generateMessage(message));
-        callback('message delivered');
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
     })
 
-
-    //inform when user goes offline
-    socket.on('disconnect', () => {
-        io.emit('message', generateMessage('User is offline'));
-    })
-
-    
-    //get cordinates of users and send it as a google map location
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage',  generateLocationMessage(`https://maps.google.com/?q=${coords.latitude},${coords.longitude}`))
-        callback('location shared!');
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
-
-
-//listen to the port and run the server
 server.listen(port, () => {
-    console.log(`Server on port ${port}!`);
+    console.log(`Server is up on port ${port}!`)
 })
