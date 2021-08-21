96a2464791e9517b8a47bb6a5abab4afcb866056
maxbehzadi82 <maxbehzadi82@gmail.com>
2021-08-21 17:33:11 +0200

    App UI improved

---
+++ public/dist/js/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById('sidebar').style.width = '300px';
+  document.getElementById('chatMain').style.marginLeft = '0';
+}
+
+function closeNav() {
+  document.getElementById('sidebar').style.width = '0';
+  document.getElementById('chatMain').style.marginLeft = '0';
+}
