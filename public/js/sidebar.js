1fd523e17cbcbf220cf670ab653fc18e274c0ae5
Max Behzadi <maxbehzadi82@gmail.com>
2021-09-18 05:52:42 +0200

    sidebar open & close func fixed

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,9 +1,20 @@
 function openNav() {
-  document.getElementById('sidebar').style.width = '300px';
-  document.getElementById('chatMain').style.marginLeft = '0';
+  document.querySelector('.chat__sidebar').style.width = '100%';
+  document.querySelector('.chat__main').style.display = 'none';
 }
 
 function closeNav() {
-  document.getElementById('sidebar').style.width = '0';
-  document.getElementById('chatMain').style.marginLeft = '0';
+  document.querySelector('.chat__sidebar').style.width = '0';
+  document.querySelector('.chat__main').style.display = 'flex';
 }
+
+// const toggle = (elem) => {
+//   elem.classList.toggle('hidden');
+// };
+// toggle(document.querySelector('.btn'));
+
+// const openNav = document.querySelector('#openNav');
+// openNav.classList.remove('hidden');
+
+// const closeNav = document.querySelector('#closeNav')
+// closeNav.classList.add('hidden')
