c1a9cbd7ab899f8de29ccb7a4f7b1c04534959ae
Max Behzadi <maxbehzadi82@gmail.com>
2021-09-21 18:03:41 +0200

    REFACTOR: improve

---
+++ rouets/index.js
@@ -0,0 +1,9 @@
+const authRoutes = require('./authRoutes');
+const chatRoutes = require('./chatRoutes');
+const profileRoutes = require('./profileRoutes');
+
+module.exports = {
+    authRoutes,
+    chatRoutes,
+    profileRoutes
+};
