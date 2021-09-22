274fdf6fc8904a05a18dfe2516e4f9a56ed6ea5e
Max Behzadi <maxbehzadi82@gmail.com>
2021-09-23 00:45:45 +0200

    ADD: webRTC with peerjs video call

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const session = require('express-session');
 const ejs = require('ejs');
 const path = require('path');
 const config = require('./config/config');
+const { v4: uuidv4 } = require('uuid');
 
 const {
   authRoutes,
   chatRoutes,
-  profileRoutes
+  profileRoutes,
+  peerRoutes,
 } = require('./rouets');
 
 const app = express();
@@ -17,6 +19,7 @@ app.set('view engine', 'ejs');
 
 const publicDirectoryPath = path.join(__dirname, 'public');
 app.use(express.static(publicDirectoryPath));
+
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -40,18 +43,29 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
 app.use(authRoutes);
 app.use(chatRoutes);
 app.use(profileRoutes);
+// app.use(peerRoutes);
+
+// app.use('/peerjs', (req, res, next) => {
+//   res.send(peerServer);
+//   next();
+// });
+app.get('/videocall', (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 
 app.post('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/');
 });
 
-app.all('*', (req, res, ) => {
+app.all('*', (req, res) => {
   res.status(404).render('404');
 });
 
--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const dotenv = require('dotenv').config({
 const db = require('./config/db');
 const io = require('./rouets/io');
 
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true,
+// });
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () =>
   console.log(`Server is up on port ${PORT}!`)
 );
-
 io.attach(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+app.use('/peerjs', peerServer);
